98937678003d58044db109419aa00cc6bb78597a
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2023-01-27 14:52:16 -0500

    form created

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+   brand: {
+      100: 'grey',
+      200: 'rebeccapurple',
+      300: 'red',
+   },
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   const theme = extendTheme({ colors });
+
+   return (
+      <ChakraProvider theme={theme}>
+         <Component {...pageProps} />
+      </ChakraProvider>
+   );
 }
